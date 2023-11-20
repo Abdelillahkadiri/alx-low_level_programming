dfca1ab162389acaf3b447b9daabbe33571ee9bb
Abdelillahkadiri <abdelillah2954@gmail.com>
2023-11-20 13:49:02 -0800

    101-print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,20 +3,13 @@
 size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 /**
-
- * looped_listint_len - Counts the number of unique nodes
-
- * in a looped listint_t linked list.
-
- * @head: A pointer to the head of the listint_t to check.
-
- *
-
- * Return: If the list is not looped - 0.
-
- * Otherwise - the number of unique nodes in the list.
-
- */
+* looped_listint_len - Counts the number of unique nodes
+* in a looped listint_t linked list.
+* @head: A pointer to the head of the listint_t to check.
+*
+* Return: If the list is not looped - 0.
+* Otherwise - the number of unique nodes in the list.
+*/
 size_t looped_listint_len(const listint_t *head)
 {
 const listint_t *tortoise, *hare;
@@ -50,16 +43,11 @@ hare = (hare->next)->next;
 return (0);
 }
 /**
-
- * print_listint_safe - Prints a listint_t list safely.
-
- * @head: A pointer to the head of the listint_t list.
-
- *
-
- * Return: The number of nodes in the list.
-
- */
+* print_listint_safe - Prints a listint_t list safely.
+* @head: A pointer to the head of the listint_t list.
+*
+* Return: The number of nodes in the list.
+*/
 size_t print_listint_safe(const listint_t *head)
 {
 size_t nodes, index = 0;
